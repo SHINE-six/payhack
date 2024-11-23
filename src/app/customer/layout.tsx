b037ed7d62b469ca149e5f65a5c073fa1df4be65
SHINE six <desmondfoo@gmail.com>
2024-11-23 22:16:14 +0800

    update customer app sidebar dir

--- src/app/customer/layout.tsx
+++ src/app/customer/layout.tsx
@@ -1,5 +1,5 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
-import { AppSidebar } from "@/components/app-sidebar"
+import { AppSidebar } from "@/app/customer/_components/app-sidebar"
 
 export default function CutomerLayout({
     children,
